6bcae944c9a4ac11d7f4d28345b96ddbf1f7533d
VXdora <naoki5336@live.jp>
2024-12-24 13:59:50 +0900

    lazygit, keymap
    lazygitのインストール処理を追加，キーマップを修正

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -14,10 +14,15 @@ vim.api.nvim_set_keymap('n', '<down>', 'gj', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Esc><Esc>', ':nohlsearch<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-j>', ':bprev<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-k>', ':bnext<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'ff', 'ggvG=', { noremap = true })
 
 -- insertモード時の設定
-vim.api.nvim_set_keymap('i', 'jj', '<Esc><Esc>', { silent = true })
+vim.api.nvim_set_keymap('i', 'fj', '<Esc><Esc>', { silent = true })
 
 -- fern
 vim.api.nvim_set_keymap('n', '<C-n>', ':Fern .<CR>', { noremap = true })
 
+-- lsp
+vim.api.nvim_set_keymap('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>lh', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>lf', '<cmd>lua vim.lsp.buf.format()<CR>', { noremap = true })
--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 bash ./bashrc/init.sh
 bash ./nvim/init.sh
+
+lazygit --version
+if [ $? -ne 0 ]; then
+  sudo dnf copr enable atim/lazygit -y
+  sudo dnf update -y
+  sudo dnf install lazygit -y
+fi
